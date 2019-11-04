9c0556aa050df510f8ef9d5f3746fe7a186ae5c9
Waqar-107 <waqar.hassan866@gmail.com>
2019-11-04 12:32:24 +0600

    intro turned into functional component

--- src/app/components/Intro/Intro.js
+++ src/app/components/Intro/Intro.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 import Colors from '../../styles/Colors';
 import styles from './IntroStyles';
 
-class Intro extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <div className={classes.intro}>
-          <div
-            style={{ color: Colors.themeFontColor }}
-            className={classes.hi}
-          >Hi, my name is</div>
+function Intro(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <div className={classes.intro}>
+        <div
+          style={{ color: Colors.themeFontColor }}
+          className={classes.hi}
+        >Hi, my name is</div>
 
-          <div
-            style={{ color: Colors.nameColor }}
-            className={classes.name}
-          >Waqar Hassan Khan</div>
+        <div
+          style={{ color: Colors.nameColor }}
+          className={classes.name}
+        >Waqar Hassan Khan</div>
 
-          <div
-            style={{ color: Colors.motoColor }}
-            className={classes.moto}
-          >I write codes to stay alive :)</div>
+        <div
+          style={{ color: Colors.motoColor }}
+          className={classes.moto}
+        >I write codes to stay alive :)</div>
 
-          <div
-            style={{ color: Colors.shortDescriptionColor }}
-            className={classes.shortDescription}
-          >I'm a final year cs undergrad student,<br /> seeking internship opportunities.</div>
+        <div
+          style={{ color: Colors.shortDescriptionColor }}
+          className={classes.shortDescription}
+        >I'm a final year cs undergrad student,<br /> seeking internship opportunities.</div>
 
-          <div className={classes.mailMe}>
-            <a className={classes.mailLink}
-              href="mailto:waqar.hassan866@gmail.com">send me owls!!</a>
-          </div>
+        <div className={classes.mailMe}>
+          <a className={classes.mailLink}
+            href="mailto:waqar.hassan866@gmail.com">send me owls!!</a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Intro.propTypes = {
