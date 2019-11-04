e4d054adb08b068ad9358a15b058f28ae252a7bc
Waqar-107 <waqar.hassan866@gmail.com>
2019-11-04 11:06:20 +0600

    unused code removed

--- src/app/layouts/baseLayout.js
+++ src/app/layouts/baseLayout.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import Home from '../components/Home/Home';
 
-const theme = createMuiTheme({
-  typography: {
-    // Use the system font instead of the default Roboto font.
-    fontFamily: '"Montserrat"',
-    useNextVariants: true,
-  },
-});
-
 const BaseLayout = () => (
   <Router>
-    <MuiThemeProvider theme={theme}>
-      <div>
-        <Switch>
-
-          {/* Unsafe paths */}
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-
-
-          {/* <Route path="*" component={NotFound} /> */}
-        </Switch>
-      </div>
-    </MuiThemeProvider>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+      </Switch>
+    </div>
   </Router>
 );
 
