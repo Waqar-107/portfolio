cd3fae32444e68a635dfb90805690a444f3caccb
Waqar-107 <waqar.hassan866@gmail.com>
2019-11-05 12:58:17 +0600

    test

--- src/app/components/Home/Home.js
+++ src/app/components/Home/Home.js
@@ -6,10 +6,10 @@ import Nav from '../Header/NavBar';
 import Intro from '../Intro/Intro';
 import SocialWeb from '../SocialLinks/socialWeb';
 import Footer from '../Footer/Footer';
-// import About from '../AboutMe/AboutMe';
-// import Work from '../WorkExp/Work';
-// import CP from '../CP/cp';
-// import Projects from '../Projects/Projects';
+import About from '../AboutMe/AboutMe';
+import Work from '../WorkExp/Work';
+import CP from '../CP/cp';
+import Projects from '../Projects/Projects';
 import Contact from '../Contact/Contact';
 
 import styles from './HomeStyle';
@@ -21,10 +21,10 @@ function Home(props) {
 			<div className={classes.nav}><Nav /></div>
 			<div className={classes.intro}><Intro /></div>
 			<div className={classes.socialWeb}><SocialWeb /></div>
-			{/* <div id="aboutMe" className={classes.commonSegment}><About /></div>
+			<div id="aboutMe" className={classes.commonSegment}><About /></div>
 			<div id="work" className={classes.commonSegment}><Work /></div>
 			<div id="problemSolving" className={classes.commonSegment}><CP /></div>
-			<div id="projects" className={classes.commonSegment}><Projects /></div> */}
+			<div id="projects" className={classes.commonSegment}><Projects /></div>
 			<div id="contactMe" className={classes.commonSegment}><Contact /></div>
 			<div className={classes.footer}><Footer /></div>
 		</div>
