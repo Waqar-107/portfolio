7f947080ad96463b5df9865d94f0b04abc7ab402
Waqar-107 <waqar.hassan866@gmail.com>
2020-06-06 20:54:09 +0600

    work updated

--- src/app/components/WorkExp/Work.js
+++ src/app/components/WorkExp/Work.js
@@ -8,24 +8,27 @@ import Colors from '../../styles/Colors';
 import styles from './WorkStyles';
 
 function Work(props) {
-  const { classes } = props;
+	const { classes } = props;
 
-  return (
-    <div className={classes.mainBody}>
-      <div className={classes.heading}>
-        <TitleHead number="02." caption="Where I've Worked" />
-      </div>
-      <div className={classes.content}
-        style={{ color: Colors.motoColor, fontSize: '22px' }}>
-        I haven't worked officially anywhere but did a project with a group of 12 peoples in a cross-platform application.
-      </div>
-    </div>
-  );
+	return (
+		<div className={classes.mainBody}>
+			<div className={classes.heading}>
+				<TitleHead number='02.' caption="Where I've Worked" />
+			</div>
+			<div
+				className={classes.content}
+				style={{ color: Colors.motoColor, fontSize: '22px' }}
+			>
+				I have been working for{' '}
+				<a href='http://mainframelabs.ltd/'>Mainframe Labs</a> as a part-time
+				junior software developer from Feb, 2020
+			</div>
+		</div>
+	);
 }
 
 Work.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
+	classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(Work);
