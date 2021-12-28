f7d3546f3456a9d2cfca8068b321efa04a964973
Waqar-107 <waqar.hassan866@gmail.com>
2021-12-28 23:02:37 +0600

    changed packages

--- src/app/layouts/baseLayout.js
+++ src/app/layouts/baseLayout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {HashRouter as Router, Switch, Route} from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
